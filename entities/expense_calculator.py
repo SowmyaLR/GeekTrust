a1134a852bb1052557e5c97927c75888f1f8317f
SowmyaLR <sowmyasonait@gmail.com>
2022-02-28 10:29:23 +0530

    intuit code challenge commit

---
+++ entities/expense_calculator.py
@@ -0,0 +1,6 @@
+class ExpenseCalculator:
+    def __init__(self):
+        self.expense_tracker = {}
+
+    def add_expense(self, person, spent_by, amnt):
+        pass
---
+++ entities/house_mates.py
@@ -0,0 +1,30 @@
+from constants.constants import HOUSEFUL, SUCCESS, MEMEBER_NOT_FOUND
+from entities.expense_calculator import ExpenseCalculator
+
+
+class HouseMates:
+    def __init__(self):
+        self.house_mates = []
+        self.expense_Cal = ExpenseCalculator()
+
+    def _is_house_ful(self):
+        return len(self.house_mates) >= 3
+
+    def move_in(self, member):
+        if self._is_house_ful():
+            print(HOUSEFUL)
+        else:
+            self.house_mates.append(member)
+            print(SUCCESS)
+
+    def _is_valid_house_mate(self, members):
+        for mem in members:
+            if mem not in self.house_mates:
+                return False
+        return True
+
+    def spend(self, amount, members):
+        if self._is_valid_house_mate(members):
+            req_amnt = amount/len(members)
+        else:
+            print(MEMEBER_NOT_FOUND)
